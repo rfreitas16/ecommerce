04062f7bcad73830b1dd5d92211f9fde4b0a5ff6
rfreitas16 <rfreitas16@hotmail.com>
2018-09-18 15:10:27 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -8,7 +8,10 @@ class Page {
 
 	private $tpl;
 	private $options = [];
-	private $defaults = ["data"=>[]];
+	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
+		"data"=>[]];
 
 	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
@@ -28,7 +31,7 @@ class Page {
 
 	$this->setData($this->options["data"]); 
 	
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 	}
 
 	private function setData($data = array())
@@ -47,7 +50,7 @@ class Page {
 
 public function __destruct(){
 
-	$this->tpl->draw("footer");
+	if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
