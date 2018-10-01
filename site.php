507ca00d1f6e3f2274de610f530946e4d33b2fd1
rfreitas16 <rfreitas16@hotmail.com>
2018-10-01 13:14:52 -0300

    site produtos

--- site.php
+++ site.php
@@ -2,13 +2,17 @@
 ///ROTA HOME DO SITE///
 
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 
 $app->get('/', function() {
-    
+
+	$products = Product::listAll();
+	    
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", ['products'=>Product::checkList($products)
+	]);
 
 });
 
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -15,6 +15,17 @@ class Product extends Model{
 
 	 	return $sql->select("SELECT * FROM  tb_products ORDER BY desproduct");
 	}
+	public static function checkList($list){
+
+
+		foreach ($list as &$row) {
+			$p = new Product();
+			$p->setData($row);
+			$row = $p->getValues();
+
+		}
+		return $list;
+	}
 	public function save()
 	{
 
@@ -51,7 +62,7 @@ class Product extends Model{
 
 		if (file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct() . ".jpg")) {
 
-			$url = "res/site/img/products/" . $this->getidproduct() . ".jpg";
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
 
 		}else{
 
