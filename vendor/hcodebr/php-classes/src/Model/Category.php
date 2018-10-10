1cd5573a48831767e65e91fd2e415039db774c1f
rfreitas16 <rfreitas16@hotmail.com>
2018-10-10 10:16:45 -0300

    Admin - Categorias - Busca e Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -96,6 +96,48 @@ class Category extends Model{
 		$sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", [':idcategory'=>$this->getidcategory(),
 			':idproduct'=>$product->getidproduct()]);
 	}
+	//PAGINACAO//
+	public static function getPage($page = 1, $itemsPerPage = 10){
+		$start = ($page - 1) * $itemsPerPage;
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * 
+			FROM  tb_categories 
+			ORDER BY descategory
+			LIMIT $start, $itemsPerPage;");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+			
+	}
+	//FILTRO DE BUSCA DA PAGINAÇÃO//
+	public static function getPagesearch($search, $page = 1, $itemsPerPage = 10){
+		$start = ($page - 1) * $itemsPerPage;
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * 
+			FROM  tb_categories 
+			WHERE descategory LIKE :search
+			ORDER BY descategory
+			LIMIT $start, $itemsPerPage;",[
+
+				':search'=>'%'.$search.'%'
+			]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+			
+	}
 
 
 
